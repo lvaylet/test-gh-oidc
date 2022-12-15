eb3b6b4e97146827a53fec57d061283db3702d7f
Laurent VAYLET <laurent.vaylet@gmail.com>
2022-12-15 14:56:15 +0000

    enable iamcredentials.googleapis.com

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,3 +37,12 @@ module "gh_oidc" {
     }
   }
 }
+
+resource "google_project_service" "project" {
+    project = var.project_id
+    for_each = toset([
+        "iamcredentials.googleapis.com",
+    ])
+    service = each.key
+    disable_dependent_services = true
+}
